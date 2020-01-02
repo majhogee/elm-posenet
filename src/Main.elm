9171da0f9ea90179e6ea0f2bdffd05e6faa6825e
majhogee <jo.wojtowicz@gmail.com>
2020-01-02 23:04:17 +0100

    initial setup

---
+++ src/Main.elm
@@ -0,0 +1,67 @@
+module Main exposing (main)
+
+import Browser
+import File exposing (File)
+import File.Select as Select
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Task
+
+
+type alias Model =
+    Maybe Int
+
+
+type Msg
+    = Increase Int
+    | Decrease Int
+    | Reset
+    | Initialize
+
+
+main =
+    Browser.sandbox
+        { init = init
+        , view = view
+        , update = update
+        }
+
+
+init : Model
+init =
+    Nothing
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increase step ->
+            Maybe.map ((+) step) model
+
+        Decrease step ->
+            Maybe.map (\count -> count - step) model
+
+        Reset ->
+            Just 0
+
+        Initialize ->
+            Just 0
+
+
+view : Model -> Html Msg
+view model =
+    case model of
+        Nothing ->
+            div []
+                [ button [ type_ "button", onClick Initialize ] [ text "Initialize" ] ]
+
+        Just count ->
+            div []
+                [ button [ type_ "button", onClick (Decrease 1) ] [ text "-" ]
+                , button [ type_ "button", onClick (Decrease 5) ] [ text "-5" ]
+                , text (String.fromInt count)
+                , button [ type_ "button", onClick (Increase 1) ] [ text "+" ]
+                , button [ type_ "button", onClick (Increase 5) ] [ text "+5" ]
+                , button [ type_ "button", onClick Reset ] [ text "Reset" ]
+                ]
